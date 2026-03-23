67f5053615aace291a67e364cb67fc8694b1acb9
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-24 03:25:27 +0530

    config ->class based

--- packages/config/env.ts
+++ packages/config/env.ts
@@ -1,12 +1,24 @@
-import {z} from "zod";
+import { z } from "zod";
 
-const envSchema = z.object({
-	DATABASE_URL: z.url(),
-	JWT_SECRET: z.string().min(8),
-	REDIS_URL: z.url(),
-	PORT:z.coerce.number().default(3000)
-});
+class Env {
+	private static instance: ReturnType<typeof Env.create>;
+	private constructor() {}
+	private	static create() {
+		return z.object({
+				DATABASE_URL: z.url(),
+				JWT_SECRET: z.string().min(8),
+				REDIS_URL: z.url(),
+				PORT:z.coerce.number().default(3000)
+		}).parse(process.env)
+	}
 
+	public static get(): ReturnType<typeof Env.create>{
+		if(!this.instance){
+			this.instance= this.create();
+		}
+		return this.instance;
+	}
+}
 
-export const env = envSchema.parse(process.env)
-
+export const env = Env.get();
+ 
