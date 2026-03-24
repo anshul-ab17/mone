b36a501873562faa982eb6638c64a36359d83003
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-24 22:22:47 +0530

    added pgAdapter

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,12 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
 class Database {
   private static instance: PrismaClient;
   private constructor() {}
-  
-  public static getInstance(): PrismaClient{
-    if(!this.instance){
-      this.instance= new PrismaClient({
-        log:process.env.NODE_ENV=="production" ?["error"]: ["query","warn","error"]
+
+  public static getInstance(): PrismaClient {
+    if (!this.instance) {
+      const pool = new Pool({
+        connectionString: process.env.DB_CONN_POOL,
+      });
+      const adapter = new PrismaPg(pool);
+      this.instance = new PrismaClient({
+        adapter,
+        log:
+          process.env.NODE_ENV === "production"
+            ? ["error"]
+            : ["query", "warn", "error"],
       });
     }
     return this.instance;
