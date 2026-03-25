640a7befe08221ab81829581644b2dbd8c658d0e
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-26 05:12:21 +0530

    added order book

---
+++ apps/engine/v1/orderbook.ts
@@ -0,0 +1,33 @@
+import { EngineOrder, Side } from "./types";
+
+export class OrderBook{
+    bids: Map<number, EngineOrder[]> = new Map(); //highest buyer first. 
+    asks: Map<number, EngineOrder[]> = new Map(); // lowest seller first.
+
+    add(order:EngineOrder){
+        const book =order.side ==="BUY"? this.bids: this.asks;
+
+        if(!book.has(order.price)){
+            book.set(order.price,[]);
+        }
+        book.get(order.price)!.push(order);
+    }
+
+    getBestBid(): number | null {
+        if(this.bids.size===0) return null;
+        return Math.max(...this.bids.keys());
+    }
+
+    getBestAsk():number | null{
+        if(this.bids.size===0) return null;
+        return Math.min(...this.asks.keys());
+    }
+
+    removeEmpty(price:number, side:Side){
+        const book =side ==="BUY"? this.bids:this.asks;
+
+        if(book.get(price)?.length===0){
+            book.delete(price);
+        }
+    }
+}
