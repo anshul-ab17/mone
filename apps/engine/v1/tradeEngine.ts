d17bb1891439d379c76e80828a3c655aed94e896
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-26 05:28:07 +0530

    added trading_engine

---
+++ apps/engine/v1/tradeEngine.ts
@@ -0,0 +1,20 @@
+import { randomUUID } from "crypto";
+import { EngineOrder, Trade } from "./types";
+
+
+export class TradeEngine{
+    execute(
+        buy:EngineOrder, sell:EngineOrder, 
+        quantity:number, price:number): Trade {
+        const trade:Trade ={
+            id:randomUUID(),
+            marketId:buy.marketId,
+            price, quantity,
+            buyOrderId:buy.id,
+            sellOrderId:sell.id,
+            timeStamp:Date.now(),
+        }
+        console.log("TRADE_EXECUTED", trade);
+        return trade;
+    }
+}
