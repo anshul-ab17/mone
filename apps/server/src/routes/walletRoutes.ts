35b4e39efbdd4195ec12b4c6670ec6a105bebc6a
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-26 01:57:43 +0530

    validating the auth in test

--- apps/server/src/routes/walletRoutes.ts
+++ apps/server/src/routes/walletRoutes.ts
@@ -2,10 +2,12 @@ import { Hono } from "hono";
 import { WalletController } from "../controllers/walletController";
 import { WalletSchema } from "@repo/types";
 import { validate } from "../middlewares/validateMiddleware";
-
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export const walletRouters = new Hono();
 const controller = new WalletController();
+
+walletRouters.use("*", authMiddleware);
 
 walletRouters.get('/', (c) => controller.getWallets(c));
 walletRouters.post(
